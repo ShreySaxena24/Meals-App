8ef6ceb42c804ebb5042c27529c4875feeb80652
Shrey Saxena <shrey.saxena.sms@gmail.com>
2022-02-22 01:15:44 +0530

    Commit1

---
+++ lib/screens/favourites_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class FavouritesScreen extends StatelessWidget {
+  final List<Meal> favouriteMeals;
+
+  FavouritesScreen(this.favouriteMeals);
+
+  // import 'package:flutter/material.dart';
+
+  //const FavouritesScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favouriteMeals.isEmpty) {
+      return Center(
+        child: Text('No Favourites added yet!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favouriteMeals[index].id,
+            title: favouriteMeals[index].title,
+            imageUrl: favouriteMeals[index].imageUrl,
+            duration: favouriteMeals[index].duration,
+            complexity: favouriteMeals[index].complexity,
+            affordability: favouriteMeals[index].affordability,
+          );
+        },
+        itemCount: favouriteMeals.length,
+      );
+    }
+  }
+}
